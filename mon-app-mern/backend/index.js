e381b62f76da008355b1243c94db5ea5780f5b2c
MP-Death30 <mathis30150@gmail.com>
2025-04-09 11:06:23 +0200

    Initial commit

---
+++ mon-app-mern/backend/index.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+mongoose.connect('mongodb://mongo:27017/test');
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from backend' });
+});
+
+app.listen(5000, () => console.log('Backend running on port 5000'));
